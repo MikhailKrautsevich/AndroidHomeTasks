30ea98ae243004549245e7aac8333552cc7fd875
Misha <mihail4egevara@gmail.com>
2020-05-05 01:13:36 +0300

    try to do homeTask_04

--- hometask_04_kotlincontacts/src/main/java/com/example/hometask_04_kotlincontacts/MainActivity.kt
+++ hometask_04_kotlincontacts/src/main/java/com/example/hometask_04_kotlincontacts/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.hometask_04_kotlincontacts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerContacts : RecyclerView
+    lateinit var linearLayoutManager: LinearLayoutManager
+    lateinit var gridLayoutManager: GridLayoutManager
+    lateinit var adapter1: NameListAdapter
+    lateinit var noContacts : TextView
+    var contacts: ArrayList<ContactClass> = ArrayList<ContactClass>()
+
+    val ADD_NEW_CONTACT = 900
+    val EDIT_CONTACT = 901
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+
---
+++ hometask_04_kotlincontacts/src/main/java/com/example/hometask_04_kotlincontacts/NameListAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.hometask_04_kotlincontacts
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NameListAdapter : RecyclerView.Adapter<NameListAdapter.ItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {}
+}
