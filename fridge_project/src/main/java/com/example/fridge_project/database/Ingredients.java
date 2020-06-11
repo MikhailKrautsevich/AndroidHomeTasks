e41c78bc342c07199f19acdce4ee650868d39d5e
Misha <mihail4egevara@gmail.com>
2020-06-12 02:03:14 +0300

    homeTask_05

---
+++ fridge_project/src/main/java/com/example/fridge_project/database/Ingredients.java
@@ -0,0 +1,51 @@
+package com.example.fridge_project.database;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity (tableName = "recipe_ingredients")
+public class Ingredients {
+    @PrimaryKey(autoGenerate = true)
+    private int id ;
+    private int food_id ;
+    private int recipe_id ;
+    private double amount ;
+
+    public Ingredients(int food_id, int recipe_id, double amount) {
+        this.food_id = food_id;
+        this.recipe_id = recipe_id;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getFood_id() {
+        return food_id;
+    }
+
+    public int getRecipe_id() {
+        return recipe_id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFood_id(int food_id) {
+        this.food_id = food_id;
+    }
+
+    public void setRecipe_id(int recipe_id) {
+        this.recipe_id = recipe_id;
+    }
+}
---
+++ fridge_project/src/main/java/com/example/fridge_project/database/Recipe.java
@@ -0,0 +1,41 @@
+package com.example.fridge_project.database;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "recipe")
+public class Recipe {
+    @PrimaryKey(autoGenerate = true)
+    private int id ;
+    private String title ;
+    private String description ;
+
+    public Recipe(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
