22720e8c1fe39fe7c80f52aa6ae6be8512862ece
Misha <mihail4egevara@gmail.com>
2020-06-14 02:36:34 +0300

    homeTask_05

---
+++ fridge_project/src/main/java/com/example/fridge_project/database/FridgeDao.java
@@ -0,0 +1,19 @@
+package com.example.fridge_project.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface FridgeDao {
+
+    @Query("SELECT * from fridge WHERE food_id = :food_id ")
+    int getCurrentAmountById(int food_id) ;
+
+    @Update
+    void updateCurrentAmountById(Food food) ;
+
+    @Delete
+    void deleteNote(Food food) ;
+}
--- fridge_project/src/main/java/com/example/fridge_project/database/Ingredients.java
+++ fridge_project/src/main/java/com/example/fridge_project/database/Ingredients.java
@@ -1,9 +1,21 @@
 package com.example.fridge_project.database;
 
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity (tableName = "recipe_ingredients")
+@Entity (tableName = "recipe_ingredients",
+        foreignKeys = { @ForeignKey
+                (entity = Recipe.class ,
+                parentColumns = "id",
+                childColumns = "recipe_id",
+                onDelete = ForeignKey.CASCADE,
+                deferred = true),
+                @ForeignKey(entity = Food.class ,
+                parentColumns = "id",
+                childColumns = "food_id" ,
+                onDelete = ForeignKey.CASCADE,
+                deferred = true) } )
 public class Ingredients {
     @PrimaryKey(autoGenerate = true)
     private int id ;
