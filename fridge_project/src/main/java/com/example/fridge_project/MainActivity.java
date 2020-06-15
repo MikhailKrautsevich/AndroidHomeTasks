0dc14a3820737c43e3f4b9323a230bfa6461d462
Misha <mihail4egevara@gmail.com>
2020-06-15 19:10:02 +0300

    try to do my project

--- fridge_project/src/main/java/com/example/fridge_project/MainActivity.java
+++ fridge_project/src/main/java/com/example/fridge_project/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.fridge_project;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.fridge_project.repo.FridgeRepository;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FridgeRepository fridgeRepository ;
+    Button goToProducts ;
+    Button goToRecipes ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initRepo(this);
+        fridgeRepository = new FridgeRepository(this);
+
+        goToProducts = findViewById(R.id.goToProducts) ;
+        goToProducts.setOnClickListener( new goToProductsListener());
+        goToRecipes = findViewById(R.id.goToRecipes) ;
+
+        fridgeRepository.getFoodDataList() ;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        closeRepo(); ;
+    }
+
+    private void initRepo(final Context context) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                fridgeRepository = new FridgeRepository(MainActivity.this) ;
+            }
+        });
+        thread.start();
+    }
+
+    private void closeRepo() {
+        fridgeRepository = null ;
+    }
+
+    class goToProductsListener implements OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this , ProductsActivity.class) ;
+            fridgeRepository.getFoodDataList() ;
+            startActivity(intent);
+        }
     }
 }
