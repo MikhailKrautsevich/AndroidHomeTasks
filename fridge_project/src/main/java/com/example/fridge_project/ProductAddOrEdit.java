df1eb250094de808fc310a7a5d43b69bf361414b
Misha <mihail4egevara@gmail.com>
2020-06-18 23:24:03 +0300

    try to do my project

--- fridge_project/src/main/java/com/example/fridge_project/ProductAddOrEdit.java
+++ fridge_project/src/main/java/com/example/fridge_project/ProductAddOrEdit.java
@@ -46,19 +46,34 @@ public class ProductAddOrEdit extends AppCompatActivity {
         });
 
         if (intent.getStringExtra(TITLE_KEY) != null) {
-            String nameCur = intent.getStringExtra(TITLE_KEY) ;
+            final String nameCur = intent.getStringExtra(TITLE_KEY) ;
             String amountCur = intent.getStringExtra(AMOUNT_KEY) ;
             Double dAmountCur = Double.parseDouble(amountCur) ;
             name.setText(nameCur);
             amount.setText(amountCur);
-            save.setText(R.string.change);
             if (dAmountCur !=null)
             {foodToWorkWith = new FoodData(nameCur, dAmountCur);}
             if (fridgeRepository == null) {
             fridgeRepository = new FridgeRepository(this) ; }
-
-
-
+            remove.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    fridgeRepository.deleteFoodByName(nameCur);;
+                    showToast("I removed this note");
+                    remove.setVisibility(View.GONE);
+                    save.setText(R.string.save);
+                    save.setOnClickListener(new SaveListener());
+                }
+            });
+            save.setText(R.string.change);
+            save.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!name.getText().equals("") &&  amount.getText().toString().matches("[0-9]+?[\\.\\,]?[0-9]*")) {
+                        fridgeRepository.changeNote(foodToWorkWith , getNameFromEdit(), getAmountFromEdit());
+                    }
+                }
+            });
         } else {
             remove.setVisibility(View.GONE);
             save.setOnClickListener(new SaveListener());
@@ -74,13 +89,13 @@ public class ProductAddOrEdit extends AppCompatActivity {
 
     private void saveFoodData(){
         if (amount.getText().toString().matches("[0-9]+?[\\.\\,]?[0-9]*")) {
-            String nameProductStr = name.getText().toString().trim();
-            String toDouble = amount.getText().toString().trim().replace(',' , '.');
-            Double amountProductDouble = Double.parseDouble(toDouble);
+            String nameProductStr = getNameFromEdit() ;
+            Double amountProductDouble = getAmountFromEdit() ;
             if (nameProductStr != null && !nameProductStr.isEmpty()
                     && amountProductDouble != null) {
                 FoodData newFood = new FoodData(nameProductStr, amountProductDouble);
-                FridgeRepository fridgeRepository = new FridgeRepository(ProductAddOrEdit.this);
+                if (fridgeRepository == null) {
+                    fridgeRepository = new FridgeRepository(this) ; }
                 fridgeRepository.addNewFood(newFood);
                 String saveMessage = String.format("Product %s in amount %.1f was saved", nameProductStr, amountProductDouble) ;
                 Toast.makeText(ProductAddOrEdit.this, saveMessage, Toast.LENGTH_LONG).show();
@@ -90,5 +105,23 @@ public class ProductAddOrEdit extends AppCompatActivity {
         } else {
             Toast.makeText(ProductAddOrEdit.this, "Введите корректное количество", Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        fridgeRepository = null ;
+    }
+
+    private void showToast(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    }
+
+    private double getAmountFromEdit() {
+        return Double.parseDouble(amount.getText().toString().replace(',' , '.')) ;
+    }
+
+    private String getNameFromEdit() {
+        return name.getText().toString().trim();
     }
 }
