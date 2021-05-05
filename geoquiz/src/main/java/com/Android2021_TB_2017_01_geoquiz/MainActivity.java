c05718ffda22941815c849bfb0eead6daf5ad9b5
Misha <mihail4egevara@gmail.com>
2021-05-05 23:58:16 +0300

    try to read TB

--- geoquiz/src/main/java/com/Android2021_TB_2017_01_geoquiz/MainActivity.java
+++ geoquiz/src/main/java/com/Android2021_TB_2017_01_geoquiz/MainActivity.java
@@ -1,5 +1,6 @@
 package com.Android2021_TB_2017_01_geoquiz;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -16,12 +18,15 @@ import com.google.android.material.snackbar.Snackbar;
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG = "QuizLog" ;
+    private static final String KEY_INDEX = "KEY_INDEX" ;
 
     private LinearLayout mainLayout;
     private Button mTrueButton;
     private Button mFalseButton;
-    private ImageButton mNextButton;
-    private ImageButton mPreviousButton;
+    private Button mNextButton;
+    private Button mPreviousButton;
+    private ImageButton mNextImageButton;
+    private ImageButton mPreviousImageButton;
 
     private TextView mQuestionTextView;
 
@@ -42,20 +47,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(LOG, "OnCreate() called") ;
 
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX) ;
+        }
+
+        mQuestionTextView = findViewById(R.id.question_text_view);
         mainLayout = findViewById(R.id.main_layout);
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
-        mNextButton = findViewById(R.id.next_button);
-        mPreviousButton = findViewById(R.id.prev_button);
-
-        mQuestionTextView = findViewById(R.id.question_text_view);
-        updateQuestion();
 
         mQuestionTextView.setOnClickListener(new GeoQuizListener());
         mTrueButton.setOnClickListener(new GeoQuizListener());
         mFalseButton.setOnClickListener(new GeoQuizListener());
-        mNextButton.setOnClickListener(new GeoQuizListener());
-        mPreviousButton.setOnClickListener(new GeoQuizListener());
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            mNextImageButton = findViewById(R.id.next_im_button);
+            mPreviousImageButton = findViewById(R.id.prev_im_button);
+
+            mNextButton = null ;
+            mPreviousButton = null ;
+
+            mNextImageButton.setOnClickListener(new GeoQuizListener());
+            mPreviousImageButton.setOnClickListener(new GeoQuizListener());
+        } else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            mNextButton = findViewById(R.id.next_button);
+            mPreviousButton = findViewById(R.id.prev_button);
+
+            mNextImageButton = null ;
+            mPreviousImageButton = null ;
+
+            mNextButton.setOnClickListener(new GeoQuizListener());
+            mPreviousButton.setOnClickListener(new GeoQuizListener());
+        }
+
+        updateQuestion();
     }
 
     @Override
@@ -86,6 +111,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(LOG, "OnDestroy() called") ;
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(LOG, "onSaveInstanceState() called") ;
+        outState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
     private void updateQuestion() {
@@ -142,11 +174,13 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 }
                 case R.id.next_button :
+                case R.id.next_im_button :
                 case R.id.question_text_view: {
                     changeIndexOfQuestion(true);
                     updateQuestion();
                     break;
                 }
+                case R.id.prev_im_button :
                 case R.id.prev_button : {
                     changeIndexOfQuestion(false);
                     updateQuestion();
