b362217dd70ff3c0f85603732220b843910e002c
Misha <mihail4egevara@gmail.com>
2021-05-10 23:24:40 +0300

    try to read TB

--- geoquiz/src/main/java/com/Android2021_TB_2017_01_geoquiz/CheatActivity.java
+++ geoquiz/src/main/java/com/Android2021_TB_2017_01_geoquiz/CheatActivity.java
@@ -1,14 +1,61 @@
 package com.Android2021_TB_2017_01_geoquiz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CheatActivity extends AppCompatActivity {
 
+    private static final String EXTRA_CHEAT = "com.Android2021_TB_2017_01_geoquiz.KEY_CHEAT" ;
+    private static final String EXTRA_ANSWER_WAS_SHOWN = "com.Android2021_TB_2017_01_geoquiz.ANSWER_WAS_SHOWN" ;
+
+    private boolean mIsAnswerTrue ;
+    private TextView mAnswerTextView ;
+    private Button mShowAnswerButton ;
+
+    public static Intent newIntent(Context context, boolean answer_is_true) {
+        Intent intent = new Intent(context, CheatActivity.class) ;
+        intent.putExtra(EXTRA_CHEAT, answer_is_true) ;
+        return intent ;
+    }
+
+    public static boolean wasAnswerShown(Intent data) {
+        return data.getBooleanExtra(EXTRA_ANSWER_WAS_SHOWN, false) ;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
+        mIsAnswerTrue = getIntent().getBooleanExtra(EXTRA_CHEAT, false) ;
+        mAnswerTextView = findViewById(R.id.answer_text_view) ;
+        mShowAnswerButton = findViewById(R.id.show_answer_button) ;
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mIsAnswerTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                setAnswerShownResult(true) ;
+            }
+        } ;
+
+        mAnswerTextView.setOnClickListener(listener);
+        mShowAnswerButton.setOnClickListener(listener);
+    }
+
+    private void setAnswerShownResult(boolean b) {
+        Intent data = new Intent() ;
+        data.putExtra(EXTRA_ANSWER_WAS_SHOWN, true) ;
+        setResult(RESULT_OK, data);
     }
 }
--- geoquiz/src/main/java/com/Android2021_TB_2017_01_geoquiz/MainActivity.java
+++ geoquiz/src/main/java/com/Android2021_TB_2017_01_geoquiz/MainActivity.java
@@ -1,5 +1,6 @@
 package com.Android2021_TB_2017_01_geoquiz;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,8 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String KEY_QUANTITY_OF_CORRECT_ANSWERS = "CORRECT" ;
     private static final String KEY_QUANTITY_OF_INCORRECT_ANSWERS = "INCORRECT" ;
     private static final String KEY_QUESTIONS = "KEY_QUESTIONS" ;
-
+    private static final int REQUEST_CODE_FOR_CHEAT = 0 ;
 
     private ViewGroup mainLayout;
     private Button mTrueButton;
@@ -41,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     private int mQCorrectAnswers = 0;
     private int mQInCorrectAnswers = 0;
     private int mQuestionsQuantity = 0;
+    private boolean mIsCheater ;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -75,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(mGeoQuizListener);
         mNextButton.setOnClickListener(mGeoQuizListener);
         mPreviousButton.setOnClickListener(mGeoQuizListener);
+        mCheatButton.setOnClickListener(mGeoQuizListener);
 
         updateQuestion();
     }
@@ -117,6 +122,19 @@ public class MainActivity extends AppCompatActivity {
         outState.putInt(KEY_QUANTITY_OF_CORRECT_ANSWERS, mQCorrectAnswers);
         outState.putInt(KEY_QUANTITY_OF_INCORRECT_ANSWERS, mQInCorrectAnswers);
         outState.putSerializable(KEY_QUESTIONS, mQuestionBank);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CODE_FOR_CHEAT) {
+                if (data != null) {
+                    mIsCheater = CheatActivity.wasAnswerShown(data) ;
+                }
+            }
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -168,6 +186,12 @@ public class MainActivity extends AppCompatActivity {
                     Snackbar.LENGTH_SHORT)
                     .setBackgroundTint(getColor(colour))
                     .show();
+            if (mIsCheater) {
+                Toast.makeText(MainActivity.this,
+                        R.string.judgment_toast,
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
         }
     }
 
@@ -250,11 +274,16 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 }
                 case R.id.cheat_button : {
+                    Log.d(LOG, "CheatActivity started") ;
+                    boolean cheat_answer = mQuestionBank[mCurrentIndex].isAnswerTrue() ;
+                    Intent intent = CheatActivity.newIntent(MainActivity.this, cheat_answer) ;
+                    startActivityForResult(intent, REQUEST_CODE_FOR_CHEAT);
                     break;
                 }
                 default: break;
             }
             updateQuestion();
+            mIsCheater = false ;
             if (mQCorrectAnswers + mQInCorrectAnswers == mQuestionsQuantity) {
                 showResults();
             }
