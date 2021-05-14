dc9f28cd56b160857a3de923096c85aac3856a89
Misha <mihail4egevara@gmail.com>
2021-05-14 23:42:54 +0300

    try to read TB

---
+++ criminalintent/src/main/java/com/example/criminalintent/Crime.java
@@ -0,0 +1,45 @@
+package com.example.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID mID ;
+    private String mTitle ;
+    private Date mDate ;
+    private boolean mSolved ;
+    
+    public Crime() {
+        mID = UUID.randomUUID() ;
+        mDate = new Date() ;
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
--- criminalintent/src/main/java/com/example/criminalintent/CrimeActivity.java
+++ criminalintent/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -1,14 +1,30 @@
 package com.example.criminalintent;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class CrimeActivity extends AppCompatActivity {
+
+    private static final String LOG = "CrimeActivity_log" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager() ;
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container) ;
+
+        if (fragment == null) {
+            fragment = new CrimeFragment() ;
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit() ;
+            Log.d(LOG, "CrimeActivity: if(fragment == null)-branch") ;
+        }
     }
 }
