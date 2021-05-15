517d986d1385a6675ed66abac49046e65425b415
Misha <mihail4egevara@gmail.com>
2021-05-15 17:37:38 +0300

    try to read TB

--- criminalintent/src/main/java/com/example/criminalintent/CrimeActivity.java
+++ criminalintent/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -1,30 +1,13 @@
 package com.example.criminalintent;
 
-import android.os.Bundle;
-import android.util.Log;
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     private static final String LOG = "CrimeActivity_log" ;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        FragmentManager fragmentManager = getSupportFragmentManager() ;
-        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container) ;
-
-        if (fragment == null) {
-            fragment = new CrimeFragment() ;
-            fragmentManager.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit() ;
-            Log.d(LOG, "CrimeActivity: if(fragment == null)-branch") ;
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
