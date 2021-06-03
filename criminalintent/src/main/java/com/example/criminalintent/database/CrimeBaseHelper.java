ec9ed74515b1e4b5d56008909c044c4930cd0cd8
Misha <mihail4egevara@gmail.com>
2021-06-03 23:55:03 +0300

    see SQLite

---
+++ criminalintent/src/main/java/com/example/criminalintent/database/CrimeBaseHelper.java
@@ -0,0 +1,20 @@
+package com.example.criminalintent.database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CrimeBaseHelper extends SQLiteOpenHelper {
+
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "crimeBase.db" ;
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
