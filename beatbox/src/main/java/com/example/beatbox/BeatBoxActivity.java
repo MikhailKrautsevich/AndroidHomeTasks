54c7897aef658d5366b5b4fdfecc854e424bcdc4
Misha <mihail4egevara@gmail.com>
2021-07-20 22:50:06 +0300

    begin

---
+++ beatbox/src/main/java/com/example/beatbox/BeatBoxActivity.java
@@ -0,0 +1,20 @@
+package com.example.beatbox;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_beat_box);
+    }
+}
