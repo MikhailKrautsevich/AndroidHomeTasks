142b3c0347436022a7b58519cb88a118d8201908
Misha <mihail4egevara@gmail.com>
2021-07-23 17:17:32 +0300

    binding

--- beatbox/src/main/java/com/example/beatbox/BeatBox.java
+++ beatbox/src/main/java/com/example/beatbox/BeatBox.java
@@ -6,21 +6,20 @@ import android.util.Log;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class BeatBox {
     private static final String TAG = "BeatBox" ;
     private static final  String SOUNDS_FOLDER = "sample_sounds" ;
 
     private AssetManager mAssetManager ;
-    private List<Sound> mSounds = new ArrayList() ;
+    private ArrayList mSounds = new ArrayList() ;
 
     public BeatBox(Context context){
         mAssetManager = context.getAssets() ;
         loadSounds() ;
     }
 
-    public List<Sound> getSounds(){return mSounds;}
+    public ArrayList<Sound> getSounds(){return mSounds;}
 
     private void loadSounds() {
         String [] soundNames ;
