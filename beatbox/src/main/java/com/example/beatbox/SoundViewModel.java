143f37185ef7bb19baa8574e07b3ce764dfb4f75
Misha <mihail4egevara@gmail.com>
2021-07-23 17:14:03 +0300

    binding

---
+++ beatbox/src/main/java/com/example/beatbox/SoundViewModel.java
@@ -0,0 +1,20 @@
+package com.example.beatbox;
+
+public class SoundViewModel {
+    private Sound mSound ;
+    private BeatBox mBeatBox ;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+    }
+
+    public String getTitle() {return mSound.getName() ;}
+}
