a2d644e907dd73fec0186b705352ab3e714b708a
Misha <mihail4egevara@gmail.com>
2021-07-29 00:18:06 +0300

    seekbar done

--- beatbox/src/main/java/com/example/beatbox/BeatBoxFragmentViewModel.java
+++ beatbox/src/main/java/com/example/beatbox/BeatBoxFragmentViewModel.java
@@ -9,25 +9,24 @@ import androidx.databinding.Bindable;
 public class BeatBoxFragmentViewModel extends BaseObservable {
 
     private final BeatBox mBeatBox;
-    private final Context mContext ;
+    private final Context mContext;
 
     public BeatBoxFragmentViewModel(BeatBox box, Context context) {
         mBeatBox = box;
-        mContext = context ;
+        mContext = context;
     }
 
     public void onValueChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
         int progress = seekBar.getProgress();
-        float newSpeed = transformToFloat(progress) ;
+        float newSpeed = transformToFloat(progress);
         mBeatBox.setSpeedPlayback(newSpeed);
         notifyPropertyChanged(BR.speedString);
     }
 
     @Bindable
     public String getSpeedString() {
-//                String toFormat = Resources.getSystem().getString(R.string.playbackSpeedText);
-                String f = mContext.getString(R.string.playbackSpeedText) ;
-        return String.format(f, ( (int) (mBeatBox.getSpeedPlayback() * 100)));
+        String f = mContext.getString(R.string.playbackSpeedText);
+        return String.format(f, ((int) (mBeatBox.getSpeedPlayback() * 100)));
     }
 
     private float transformToFloat(int progress) {
