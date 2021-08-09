56874e7f47c3e04dcf36ed48863ccc94b1effbe5
Misha <mihail4egevara@gmail.com>
2021-08-09 23:57:53 +0300

    chapter 25 a bit in evening

--- photo_gallery/src/main/java/com/example/photo_gallery/FlickrFetchr.java
+++ photo_gallery/src/main/java/com/example/photo_gallery/FlickrFetchr.java
@@ -8,30 +8,32 @@ import java.net.URL;
 
 public class FlickrFetchr {
 
+    private static final String TAG = "FlickrFetchr";
+    private static final String KEY_API = "ced9274278907edee91ca0c4da74fe68";
+
     public byte[] getUrlBytes(String urlSpec) throws IOException {
-        URL url = new URL(urlSpec) ;
+        URL url = new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
         try {
-            ByteArrayOutputStream out = new ByteArrayOutputStream() ;
-            InputStream in = connection.getInputStream() ;
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
             if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new IOException(connection.getResponseMessage() + " with: " + urlSpec) ;
+                throw new IOException(connection.getResponseMessage() + " with: " + urlSpec);
             }
-            int bytesRead = 0 ;
-            byte[] buffer = new byte[1024] ;
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
             while ((bytesRead = in.read(buffer)) > 0) {
                 out.write(buffer, 0, bytesRead);
             }
             out.close();
-            return out.toByteArray() ;
-        }
-        finally {
+            return out.toByteArray();
+        } finally {
             connection.disconnect();
         }
     }
 
-    public String getUrlString(String urlSpec) throws IOException{
-        return new String(getUrlBytes(urlSpec)) ;
+    public String getUrlString(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
     }
 }
