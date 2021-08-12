d9fec8aa4c0c232e257f7d18f16f572f2d0e3fc8
Misha <mihail4egevara@gmail.com>
2021-08-12 23:22:07 +0300

    chapter 25 PhotoHolder

--- photo_gallery/src/main/java/com/example/photo_gallery/PhotoGalleryFragment.java
+++ photo_gallery/src/main/java/com/example/photo_gallery/PhotoGalleryFragment.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class PhotoGalleryFragment extends Fragment {
     private static final String TAG = "PhotoGalleryFragment";
@@ -35,8 +39,46 @@ public class PhotoGalleryFragment extends Fragment {
         return v;
     }
 
-    private class FetchItemTask extends AsyncTask<Void, Void, Void> {
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView ;
 
+        public PhotoHolder(View itemView) {
+            super(itemView);
+
+            mTitleTextView = (TextView) itemView ;
+        }
+
+        public void bindGalleryItem(GalleryItem item) {
+            mTitleTextView.setText(item.toString());
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+        private List<GalleryItem> mItems ;
+
+        public PhotoAdapter(List<GalleryItem> items) {
+            mItems = items;
+        }
+
+        @NonNull
+        @Override
+        public PhotoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            TextView textView = new TextView(getActivity()) ;
+            return new PhotoHolder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(PhotoHolder holder, int position) {
+            holder.bindGalleryItem(mItems.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItems.size();
+        }
+    }
+
+    private class FetchItemTask extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
             new FlickrFetchr().fetchItems();
